29ca6dd7cc8e85cdda8f18b983dbb84fe7203e75
Inieke Sendy <ini.e.sendy@gmail.com>
2022-12-31 05:36:25 +0100

    [dev]: Add Widget component and Style

---
+++ src/components/Widgets/Widget.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './widget.scss';
+
+const Widget = () => {
+  return (
+    <div className="widget__wrapper">
+      <div>Widget Left</div>
+      <div>Widget right</div>
+    </div>
+  );
+};
+
+export default Widget;
